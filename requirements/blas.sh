40c919c0147dc7bf6d734db7e6643866ee43ba19
Kelly <kwailamchan@hotmail.com>
2017-08-10 17:50:41 +0800

    [master] configured openblas. 	new file:   benchmarks/lib/openblas/include/cblas.h 	new file:   benchmarks/lib/openblas/include/f77blas.h 	new file:   benchmarks/lib/openblas/include/lapacke.h 	new file:   benchmarks/lib/openblas/include/lapacke_config.h 	new file:   benchmarks/lib/openblas/include/lapacke_example_aux.h 	new file:   benchmarks/lib/openblas/include/lapacke_mangling.h 	new file:   benchmarks/lib/openblas/include/lapacke_utils.h 	new file:   benchmarks/lib/openblas/include/openblas_config.h 	new file:   benchmarks/lib/openblas/lib/libopenblas.a         new file:   benchmarks/lib/openblas/lib/libopenblas.so 	new file:   benchmarks/lib/openblas/lib/libopenblas.so.0 	new file:   benchmarks/lib/openblas/lib/libopenblas.so.0.3 	new file:   benchmarks/lib/openblas/lib/pkgconfig/openblas.pc 	modified:   requirements/blas.sh

--- requirements/blas.sh
+++ requirements/blas.sh
@@ -3,6 +3,15 @@ git clone https://github.com/CNugteren/CLBlast.git
 cd CLBlast
 mkdir _build
 cd _build
-cmake -DCMAKE_INSTALL_PREFIX=benchmarks/lib/clblast
+cmake -DCMAKE_INSTALL_PREFIX=benchmarks/lib/clblast ..
+make -j8
+make install
+
+# openblas
+git clone https://github.com/xianyi/OpenBLAS.git
+cd OpenBLAS
+mkdir _build
+cd _build
+cmake -DCMAKE_INSTALL_PREFIX=benchmarks/lib/openblas ..
 make -j8
 make install
