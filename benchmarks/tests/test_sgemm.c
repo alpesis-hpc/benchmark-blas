aedf90ae2c626c89998c6a6eb09c29d380f99f30
Kelly <kwailamchan@hotmail.com>
2017-08-10 14:56:05 +0800

    [master] created test_clblast in test_sgemm. 	modified:   benchmarks/tests/test_sgemm.c

--- benchmarks/tests/test_sgemm.c
+++ benchmarks/tests/test_sgemm.c
@@ -6,14 +6,30 @@
 #include "benchmarks/sgemm.h"
 
 
-int main(void) {
-
+void test_clblast (sgemm_data_cpu * data_cpu)
+{
   devices_cl * nvidia = (devices_cl*)malloc(sizeof(devices_cl));
   devices_cl_init (nvidia);
   
   engine_cl * nvidia_engine = (engine_cl*)malloc(sizeof(engine_cl));
   engine_cl_init (nvidia_engine, nvidia);
+  
+  // clblast
+  sgemm_data_cl * data_cl = (sgemm_data_cl*)malloc(sizeof(sgemm_data_cl));
+  sgemm_clblast_init (nvidia_engine, data_cl, data_cpu);
+  double tic = wtime();
+  sgemm_clblast_compute (nvidia_engine, data_cl, data_cpu);
+  double toc = wtime();
+  printf ("(sgemm)(clblast) time collapsed: %f\n", toc - tic);
 
+  // clean up
+  sgemm_data_cl_del (data_cl);
+  engine_cl_del (nvidia_engine);
+  devices_cl_del (nvidia);
+}
+
+
+int main(void) {
   const float alpha = 0.7f;
   const float beta = 1.0f;
   const size_t m = 128;
@@ -25,22 +41,8 @@ int main(void) {
   sgemm_data_cpu * data_cpu = (sgemm_data_cpu*)malloc(sizeof(sgemm_data_cpu));
   sgemm_data_cpu_init (data_cpu, alpha, beta, m, n, k, lda, ldb, ldc);
 
-  // for (size_t i=0; i<m*k; ++i) { host_a[i] = 12.193f; }
-  // for (size_t i=0; i<n*k; ++i) { host_b[i] = -8.199f; }
-  // for (size_t i=0; i<m*n; ++i) { host_c[i] = 0.0f; }
-
-  sgemm_data_cl * data_cl = (sgemm_data_cl*)malloc(sizeof(sgemm_data_cl));
-  sgemm_clblast_init (nvidia_engine, data_cl, data_cpu);
-
-  double tic = wtime();
-  sgemm_clblast_compute (nvidia_engine, data_cl, data_cpu);
-  double toc = wtime();
-  printf ("(sgemm)(clblast) time collapsed: %f\n", toc - tic);
-
-  // clean up
-  sgemm_data_cl_del (data_cl);
-  engine_cl_del (nvidia_engine);
-  devices_cl_del (nvidia);
+  test_clblast (data_cpu);
+  
   sgemm_data_cpu_del (data_cpu);
 
   return 0;
