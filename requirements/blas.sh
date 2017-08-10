c016afa26cc98eeb05fb7f78914da7ba196d8f2d
Kelly <kwailamchan@hotmail.com>
2017-08-10 12:23:32 +0800

    [master] updated the installed path in blas. 	modified:   requirements/blas.sh

--- requirements/blas.sh
+++ requirements/blas.sh
@@ -3,6 +3,6 @@ git clone https://github.com/CNugteren/CLBlast.git
 cd CLBlast
 mkdir _build
 cd _build
-cmake -DCMAKE_INSTALL_PREFIX=/path/to/install
+cmake -DCMAKE_INSTALL_PREFIX=benchmarks/lib/clblast
 make -j8
 make install
